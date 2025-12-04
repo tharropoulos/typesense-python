ad2a7fdc0f73467be42759f513a91fbb171ef18f
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-12-04 16:28:44 +0200

    test(synonym_set): add missing integration tests

--- tests/synonym_set_items_test.py
+++ tests/synonym_set_items_test.py
@@ -3,13 +3,11 @@
 from __future__ import annotations
 
 import pytest
-import requests_mock
 
 from tests.utils.version import is_v30_or_above
 from typesense.client import Client
-from typesense.synonym_set import SynonymSet
+from typesense.synonym_sets import SynonymSets
 from typesense.types.synonym_set import (
-    SynonymItemDeleteSchema,
     SynonymItemSchema,
 )
 
@@ -26,13 +24,62 @@ pytestmark = pytest.mark.skipif(
 )
 
 
+def test_actual_list_items(
+    actual_synonym_sets: SynonymSets,
+    delete_all_synonym_sets: None,
+    create_synonym_set: None,
+) -> None:
+    """Test that the SynonymSet object can list items from Typesense Server."""
+    response = actual_synonym_sets["test-set"].list_items()
+
+    assert response == [
+        {
+            "id": "company_synonym",
+            "root": "",
+            "synonyms": ["companies", "corporations", "firms"],
+        },
     ]
 
 
+def test_actual_get_item(
+    actual_synonym_sets: SynonymSets,
+    delete_all_synonym_sets: None,
+    create_synonym_set: None,
+) -> None:
+    """Test that the SynonymSet object can get a specific item from Typesense Server."""
+    response = actual_synonym_sets["test-set"].get_item("company_synonym")
+
+    assert response == {
+        "id": "company_synonym",
+        "root": "",
+        "synonyms": ["companies", "corporations", "firms"],
     }
 
 
+def test_actual_upsert_item(
+    actual_synonym_sets: SynonymSets,
+    delete_all_synonym_sets: None,
+    create_synonym_set: None,
+) -> None:
+    """Test that the SynonymSet object can upsert an item in Typesense Server."""
     payload: SynonymItemSchema = {
+        "id": "brand_synonym",
+        "synonyms": ["brand", "brands", "label"],
+    }
+    response = actual_synonym_sets["test-set"].upsert_item("brand_synonym", payload)
+
+    assert response == {
+        "id": "brand_synonym",
+        "synonyms": ["brand", "brands", "label"],
     }
 
 
+def test_actual_delete_item(
+    actual_synonym_sets: SynonymSets,
+    delete_all_synonym_sets: None,
+    create_synonym_set: None,
+) -> None:
+    """Test that the SynonymSet object can delete an item from Typesense Server."""
+    response = actual_synonym_sets["test-set"].delete_item("company_synonym")
+
+    assert response == {"id": "company_synonym"}
