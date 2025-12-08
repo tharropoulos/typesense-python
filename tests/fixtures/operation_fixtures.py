e3e6c82ff6f3dbc443d05c52ebbecafc7a47618b
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-12-08 20:12:31 +0200

    feat(ops): add async support for operations
    - add AsyncOperations class for async operations functionality
    - add async tests for operations functionality
    - add async fixtures for testing async operations

--- tests/fixtures/operation_fixtures.py
+++ tests/fixtures/operation_fixtures.py
@@ -3,6 +3,8 @@
 import pytest
 
 from typesense.api_call import ApiCall
+from typesense.async_api_call import AsyncApiCall
+from typesense.async_operations import AsyncOperations
 from typesense.operations import Operations
 
 
@@ -16,3 +18,19 @@ def actual_operations_fixture(actual_api_call: ApiCall) -> Operations:
 def fake_operations_fixture(fake_api_call: ApiCall) -> Operations:
     """Return a Collection object with test values."""
     return Operations(fake_api_call)
+
+
+@pytest.fixture(scope="function", name="actual_async_operations")
+def actual_async_operations_fixture(
+    actual_async_api_call: AsyncApiCall,
+) -> AsyncOperations:
+    """Return a AsyncOperations object using a real API."""
+    return AsyncOperations(actual_async_api_call)
+
+
+@pytest.fixture(scope="function", name="fake_async_operations")
+def fake_async_operations_fixture(
+    fake_async_api_call: AsyncApiCall,
+) -> AsyncOperations:
+    """Return a AsyncOperations object with test values."""
+    return AsyncOperations(fake_async_api_call)
--- tests/operations_test.py
+++ tests/operations_test.py
@@ -6,12 +6,14 @@ import pytest
 
 from tests.utils.object_assertions import assert_match_object, assert_object_lists_match
 from typesense.api_call import ApiCall
+from typesense.async_api_call import AsyncApiCall
+from typesense.async_operations import AsyncOperations
 from typesense.exceptions import ObjectNotFound
 from typesense.operations import Operations
 
 
 def test_init(fake_api_call: ApiCall) -> None:
-    """Test that the Override object is initialized correctly."""
+    """Test that the Operations object is initialized correctly."""
     operations = Operations(fake_api_call)
 
     assert_match_object(operations.api_call, fake_api_call)
@@ -22,6 +24,24 @@ def test_init(fake_api_call: ApiCall) -> None:
     assert_match_object(
         operations.api_call.config.nearest_node,
         fake_api_call.config.nearest_node,
+    )
+    assert (
+        operations._endpoint_path("resource") == "/operations/resource"  # noqa: WPS437
+    )
+
+
+def test_init_async(fake_async_api_call: AsyncApiCall) -> None:
+    """Test that the AsyncOperations object is initialized correctly."""
+    operations = AsyncOperations(fake_async_api_call)
+
+    assert_match_object(operations.api_call, fake_async_api_call)
+    assert_object_lists_match(
+        operations.api_call.node_manager.nodes,
+        fake_async_api_call.node_manager.nodes,
+    )
+    assert_match_object(
+        operations.api_call.config.nearest_node,
+        fake_async_api_call.config.nearest_node,
     )
     assert (
         operations._endpoint_path("resource") == "/operations/resource"  # noqa: WPS437
@@ -80,3 +100,59 @@ def test_invalid_operation(actual_operations: Operations) -> None:
     """Test that the Operations object throws an error for an invalid operation."""
     with pytest.raises(ObjectNotFound):
         actual_operations.perform("invalid")
+
+
+async def test_vote_async(actual_async_operations: AsyncOperations) -> None:
+    """Test that the AsyncOperations object can perform the vote operation."""
+    response = await actual_async_operations.perform("vote")
+
+    # It will error on single node clusters if asserted to True
+    assert response["success"] is not None
+
+
+async def test_db_compact_async(actual_async_operations: AsyncOperations) -> None:
+    """Test that the AsyncOperations object can perform the db/compact operation."""
+    response = await actual_async_operations.perform("db/compact")
+
+    assert response["success"]
+
+
+async def test_cache_clear_async(actual_async_operations: AsyncOperations) -> None:
+    """Test that the AsyncOperations object can perform the cache/clear operation."""
+    response = await actual_async_operations.perform("cache/clear")
+
+    assert response["success"]
+
+
+async def test_snapshot_async(actual_async_operations: AsyncOperations) -> None:
+    """Test that the AsyncOperations object can perform the snapshot operation."""
+    response = await actual_async_operations.perform(
+        "snapshot",
+        {"snapshot_path": "/tmp"},  # noqa: S108
+    )
+
+    assert response["success"]
+
+
+async def test_health_async(actual_async_operations: AsyncOperations) -> None:
+    """Test that the AsyncOperations object can perform the health operation."""
+    response = await actual_async_operations.is_healthy()
+
+    assert response
+
+
+async def test_log_slow_requests_time_ms_async(
+    actual_async_operations: AsyncOperations,
+) -> None:
+    """Test that the AsyncOperations object can perform the log_slow_requests_time_ms operation."""
+    response = await actual_async_operations.toggle_slow_request_log(
+        {"log_slow_requests_time_ms": 100},
+    )
+
+    assert response["success"]
+
+
+async def test_invalid_operation_async(actual_async_operations: AsyncOperations) -> None:
+    """Test that the AsyncOperations object throws an error for an invalid operation."""
+    with pytest.raises(ObjectNotFound):
+        await actual_async_operations.perform("invalid")
